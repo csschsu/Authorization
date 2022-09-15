b93c5fd02b4428c7985f7b3810b9ad4f0f3cd8a4
csschsu <christer.sundgren@systemkonstruktion.se>
2022-09-15 19:30:53 +0200

    Added post method

--- src/main/java/com/stabilizer/authorization/AuthorizationController.java
+++ src/main/java/com/stabilizer/authorization/AuthorizationController.java
@@ -1,6 +1,7 @@
 package com.stabilizer.authorization;
 
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,22 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.tmf632.model.*;
 import io.swagger.tmf669.model.*;
 
-
 @RestController
 public class AuthorizationController {
     @RequestMapping(value = "/hello")
     public String hello() {
-        Individual individ = new Individual();
-        individ.setFamilyName("Christer");
+        Individual individual = new Individual();
+        individual.setFamilyName("Christer");
         PartyRole pr = new PartyRole();
         pr.name("administrator");
-        return "Hello World " + individ.getFamilyName() + " " + pr.getName();
+        return "Hello World " + individual.getFamilyName() + " " + pr.getName();
     }
     @PostMapping("/createparty")
-    public String createParty(@RequestBody String familyName) {
-        Individual individ = new Individual();
-        individ.setFamilyName(familyName);
-        return "Hello " + individ.getFamilyName() +"\n";
+    public String createParty(@RequestBody String request) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Individual individual = mapper.readValue(request, Individual.class);
+        return "Hello " + individual.getFamilyName() +"\n";
     }
-
 }
