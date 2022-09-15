afed05af52389cb2e00e40e6b7347375aaa4da2a
csschsu <christer.sundgren@systemkonstruktion.se>
2022-09-15 20:32:01 +0200

    Added mongoDB

--- src/main/java/com/stabilizer/authorization/AuthorizationController.java
+++ src/main/java/com/stabilizer/authorization/AuthorizationController.java
@@ -2,12 +2,19 @@ package com.stabilizer.authorization;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.tmf632.model.*;
 import io.swagger.tmf669.model.*;
+import java.util.Random;
 
 @RestController
 public class AuthorizationController {
@@ -19,10 +26,37 @@ public class AuthorizationController {
         pr.name("administrator");
         return "Hello World " + individual.getFamilyName() + " " + pr.getName();
     }
+
     @PostMapping("/createparty")
     public String createParty(@RequestBody String request) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Individual individual = mapper.readValue(request, Individual.class);
-        return "Hello " + individual.getFamilyName() +"\n";
+        return "Hello " + individual.getFamilyName() + "\n";
     }
-}
+
+    @PostMapping("/createindividual")
+    public String createindividual(@RequestBody String request) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Individual individual = mapper.readValue(request, Individual.class);
+        insert(individual.getFamilyName());
+        return "Done\n";
+    }
+
+    public void insert(String data) {
+        try (MongoClient mongoClient = MongoClients.create(connectionString())) {
+
+            MongoDatabase sampleTrainingDB = mongoClient.getDatabase("sample_training");
+            MongoCollection<Document> gradesCollection = sampleTrainingDB.getCollection("inventory");
+
+            Random rand = new Random();
+            Document student = new Document("_id", new ObjectId());
+            student.append("item", 20003d)
+                    .append("qty", 200d)
+            .append("data", data);
+            gradesCollection.insertOne(student);
+        }
+    }
+            public static String connectionString (){
+            return "mongodb://localhost:27017";
+            }
+    }
